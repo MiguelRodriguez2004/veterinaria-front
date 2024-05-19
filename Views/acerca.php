64d43b2690a309f04a62b8f339b80f4bc949460c
MiguelRodriguez2004 <miguelangelnunezrodriguez2004@gmail.com>
2024-05-18 19:44:17 -0500

    upload document acerca.php

--- Views/acerca.php
+++ Views/acerca.php
@@ -3,14 +3,6 @@
 <?php require('./layout/topbar.php'); ?>
 <?php require('./layout/sidebar.php'); ?>
 
-<?php
-// session_start();
-// if (empty($_SESSION['user']) and empty($_SESSION['clave'])) {
-//     header('location:login/login.php');
-// }
-
-?>
-
 <div class="page-content">
 
 <div>
